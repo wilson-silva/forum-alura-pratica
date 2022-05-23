c077b56b1bfdd4e9c3833ee1eedbabca7d748b39
Wilson <wilson-gs@hotmail.com>
2022-05-23 17:27:01 -0300

    criado classe de teste da Controller Autenticação

---
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,41 @@
+package br.com.alura.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AutenticacaoControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void deveriaDevolver400CasoDadosDeAutenticacaoEstejamIncorretos() throws Exception {
+        URI uri = new URI("/auth");
+        String json = "{\"emal\":\"invalido@email.com\", \"senha\":\"123456}\"";
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(uri)
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(400));
+    }
+
+
+}
